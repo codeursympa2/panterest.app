dffc56948b4a24dd12df0c8c5e31dadc4a3ce9c5
Anrichidine <abdouanjouanais@gmail.com>
2021-04-22 22:41:08 +0000

    Fin du quatrieme video, gestion du front de l'application avec webpack et oprimisation à la fin du code en mode production

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import './styles/test.css';
 // start the Stimulus application
+import $ from 'jquery';
 import './bootstrap';
