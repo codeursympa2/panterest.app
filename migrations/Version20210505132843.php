ad8c1e40fbee10cc754f288ccd8c4c578d0fb5e2
Anrichidine Abdallah <abdouanjouanais@gmail.com>
2021-05-05 16:29:26 +0000

    Création de l'entité utilisateur avec symfony et ajustement de la rondeur de l'image au passage de la sourie

---
+++ migrations/Version20210505132843.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210505132843 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Creation des utilisateur';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL,
+         first_name VARCHAR(255) NOT NULL,
+          last_name VARCHAR(255) NOT NULL,
+           email VARCHAR(180) NOT NULL,
+            roles LONGTEXT NOT NULL COMMENT \'(DC2TYPE:json)\', password VARCHAR(255) NOT NULL,
+             created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+              updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+               UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE users');
+    }
+}
